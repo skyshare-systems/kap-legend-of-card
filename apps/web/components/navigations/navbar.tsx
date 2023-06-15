7e241458c3457ea83d207eeee550a4e7cb3a24ac
Vince Irving Lucas <sikatunar@gmail.com>
2023-06-15 21:48:25 +0800

    Add fonts

---
+++ apps/web/components/navigations/navbar.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Lato } from "next/font/google";
+const lato = Lato({
+  subsets: ["latin"],
+  weight: "100",
+});
+
+const NavbarPage = () => {
+  const socmed = [
+    {
+      id: 1,
+      name: "Home",
+      linkUrl: "https://legendofcards.io/",
+    },
+    {},
+  ];
+  return <div className="bg-black">asd</div>;
+};
+
+export default NavbarPage;
